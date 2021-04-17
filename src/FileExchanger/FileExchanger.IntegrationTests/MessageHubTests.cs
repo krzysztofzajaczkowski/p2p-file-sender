10e55256cb25094b5f23d3b487d9c24f160f0f76
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-17 08:52:10 +0200

    Add tests for MessageHub ConnectAsHost method

--- src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
+++ src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
@@ -83,5 +83,73 @@ namespace FileExchanger.IntegrationTests
             isHost.Should().BeFalse();
         }
 
+        [Fact]
+        public async Task Message_hub_should_reply_true_when_client_invoke_connect_as_host_and_check_if_im_host_method()
+        {
+            // Arrange
+            var messageReceivedEvent = new ManualResetEvent(false);
+            bool? isHost = null;
+
+            var server = BuildTestServer();
+            var client = BuildHubConnection(server);
+
+            client.On<bool>("ReceiveCheckIfImHost", msg =>
+            {
+                isHost = msg;
+                messageReceivedEvent.Set();
+            });
+
+            // Act
+            await client.StartAsync();
+            await client.InvokeAsync("ConnectAsHost");
+            await client.InvokeAsync("CheckIfImHost");
+
+            messageReceivedEvent.WaitOne();
+
+            // Assert
+            isHost.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Message_hub_should_reply_true_to_first_client_and_false_to_second_client_when_invoke_check_if_im_host_method()
+        {
+            // Arrange
+            var messageReceivedInFirstClientEvent = new ManualResetEvent(false);
+            var messageReceivedInSecondClientEvent = new ManualResetEvent(false);
+
+            bool? isFirstClientHost = null;
+            bool? isSecondClientHost = null;
+
+            var server = BuildTestServer();
+            var firstClient = BuildHubConnection(server);
+            var secondClient = BuildHubConnection(server);
+
+            firstClient.On<bool>("ReceiveCheckIfImHost", msg =>
+            {
+                isFirstClientHost = msg;
+                messageReceivedInFirstClientEvent.Set();
+            });
+
+            secondClient.On<bool>("ReceiveCheckIfImHost", msg =>
+            {
+                isSecondClientHost = msg;
+                messageReceivedInSecondClientEvent.Set();
+            });
+
+            // Act
+            await firstClient.StartAsync();
+            await firstClient.InvokeAsync("ConnectAsHost");
+            await firstClient.InvokeAsync("CheckIfImHost");
+
+            await secondClient.StartAsync();
+            await secondClient.InvokeAsync("CheckIfImHost");
+
+            messageReceivedInFirstClientEvent.WaitOne();
+            messageReceivedInSecondClientEvent.WaitOne();
+
+            // Assert
+            isFirstClientHost.Should().BeTrue();
+            isSecondClientHost.Should().BeFalse();
+        }
     }
 }
