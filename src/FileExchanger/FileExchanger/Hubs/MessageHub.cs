0068b3b41ca6ad314a90740c4f2732e01dc6c212
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-17 08:48:24 +0200

    Implement SendMessageToAll in MessageHub

--- src/FileExchanger/FileExchanger/Hubs/MessageHub.cs
+++ src/FileExchanger/FileExchanger/Hubs/MessageHub.cs
@@ -8,6 +8,9 @@ namespace FileExchanger.Hubs
 {
     public class MessageHub : Hub
     {
-        
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceiveSendMessageToAll", message);
+        }
     }
 }
