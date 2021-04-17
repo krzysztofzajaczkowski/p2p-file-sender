51675288a1cfe2401c0ab8faa64f1af8998c6322
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-17 08:44:25 +0200

    Add helper methods to MessageHubTests

--- src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
+++ src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
 
 namespace FileExchanger.IntegrationTests
 {
     public class MessageHubTests
     {
+        private TestServer BuildTestServer()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseStartup<Startup>();
+            return new TestServer(webHostBuilder);
+        }
+
+        private HubConnection BuildHubConnection(TestServer server)
+        {
+            return new HubConnectionBuilder()
+                .WithUrl(
+                    "http://localhost/messages",
+                    c => c.HttpMessageHandlerFactory = _ => server.CreateHandler())
+                .Build();
+        }
+
     }
 }
