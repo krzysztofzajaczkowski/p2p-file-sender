f3a587464e48a8f276e32cbe96f662dafc46b872
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-17 09:25:39 +0200

    Add test and implement methods for full data exchange process

--- src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
+++ src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
@@ -192,5 +192,85 @@ namespace FileExchanger.IntegrationTests
             firstClientIsHost.Should().BeTrue();
             receivedMessage.Should().Be(message);
         }
+
+        [Fact]
+        public async Task First_client_should_receive_all_data_successfully()
+        {
+            // Arrange
+            var fileName = "testFileName.txt";
+            var message =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur interdum quis enim a maximus. " +
+                "Nulla vel leo at sapien vehicula commodo. Sed quis ipsum metus. Vivamus feugiat nisl vulputate suscipit volutpat. " +
+                "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis nec ornare sapien. " +
+                "Pellentesque et pretium erat. Suspendisse a commodo est. Nunc ut dolor.";
+            var receivedFileName = string.Empty;
+            var receivedNumberOfPackagesToSend = 0;
+            var receivedPackages = 0;
+            var receivedData = new List<byte>();
+            int numberOfPackagesToSend;
+            var buffer = new byte[2048];
+            var readOffset = 0;
+            var packageNumber = 0;
+            var str = new MemoryStream(Encoding.UTF8.GetBytes(message));
+            long remainingBytesToRead;
+
+            var hostSetEvent = new ManualResetEvent(false);
+            var startSendEvent = new ManualResetEvent(false);
+            var stopSendEvent = new ManualResetEvent(false);
+
+            var server = BuildTestServer();
+            var firstClient = BuildHubConnection(server);
+            var secondClient = BuildHubConnection(server);
+
+            firstClient.On<bool>("ReceiveConnectAsHost", msg =>
+            {
+                hostSetEvent.Set();
+            });
+
+            firstClient.On<int, string>("ReceiveStartSending", (i, s) =>
+            {
+                receivedNumberOfPackagesToSend = i;
+                receivedFileName = s;
+                startSendEvent.Set();
+            });
+
+            firstClient.On<int, byte[]>("ReceiveSendPackage", (i, bytes) =>
+            {
+                ++receivedPackages;
+                receivedData.AddRange(bytes);
+            });
+
+            firstClient.On("ReceiveStopSending", () =>
+            {
+                stopSendEvent.Set();
+            });
+
+            // Act
+            await firstClient.StartAsync();
+            await secondClient.StartAsync();
+            await firstClient.SendAsync("ConnectAsHost");
+            hostSetEvent.WaitOne();
+
+            numberOfPackagesToSend = (int)Math.Ceiling(str.Length / (decimal)buffer.Length);
+            await secondClient.SendAsync("StartSending", numberOfPackagesToSend, fileName);
+            startSendEvent.WaitOne();
+
+            remainingBytesToRead = str.Length;
+            while (remainingBytesToRead > 0)
+            {
+                var read = str.Read(buffer, readOffset, buffer.Length);
+                remainingBytesToRead -= read;
+                await secondClient.SendAsync("SendPackage", packageNumber, buffer.Take(read).ToArray());
+            }
+            await secondClient.SendAsync("StopSending");
+            stopSendEvent.WaitOne();
+
+            // Assert
+            Encoding.UTF8.GetString(receivedData.ToArray()).Should().Be(message);
+            receivedData.ToArray().Should().Equal(Encoding.UTF8.GetBytes(message));
+            receivedNumberOfPackagesToSend.Should().Be(numberOfPackagesToSend);
+            receivedPackages.Should().Be(numberOfPackagesToSend);
+            receivedFileName.Should().Be(fileName);
+        }
     }
 }
