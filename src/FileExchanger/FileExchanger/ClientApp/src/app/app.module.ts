1cc6d1c4f1d5288a8f10ef628fb916eec48e8545
Rafal Kulik <rafal.kulik@intel.com>
2021-04-20 23:38:08 +0200

    feature: added home page and removed unused components

--- src/FileExchanger/FileExchanger/ClientApp/src/app/app.module.ts
+++ src/FileExchanger/FileExchanger/ClientApp/src/app/app.module.ts
@@ -5,27 +5,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {MatSnackBarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    CounterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'counter', component: CounterComponent }
     ])
   ],
   providers: [],
--- src/FileExchanger/FileExchanger/ClientApp/src/app/home/home.component.ts
+++ src/FileExchanger/FileExchanger/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  constructor(private router: Router, private _snackBar: MatSnackBar) {
+  }
+
+  password: string;
+
+  login() {
+    if (this.password) {
+      this.router.navigate(['/counter'])
+        .then(() => console.log('Navigated to counter'));
+    } else {
+      this._snackBar.open('Log in failed...', 'Close', {
+        duration: 2000
+      });
+    }
+  }
+
 }
