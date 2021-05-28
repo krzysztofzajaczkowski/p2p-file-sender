a9c700072e5f24a68e05a48354e2511ef18cfdd7
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 00:17:24 +0200

    Add IFileManager interface

---
+++ src/FileExchanger/FileExchanger/Services/FileManager/IFileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.FileManager
+{
+    public interface IFileManager
+    {
+        void SaveFile(byte[] data, string fileName);
+        byte[] GetFile(string fileName);
+        void DeleteFile(string fileName);
+        void DeleteAllFiles();
+    }
+}
