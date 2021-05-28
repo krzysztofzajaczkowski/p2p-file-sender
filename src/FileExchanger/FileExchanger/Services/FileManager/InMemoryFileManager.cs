b14fa36e102195684b88e88af4c4a335ac72f936
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 00:17:53 +0200

    Add InMemory and Directory FileManager implementations

---
+++ src/FileExchanger/FileExchanger/Services/FileManager/InMemoryFileManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.FileManager
+{
+    public class InMemoryFileManager : IFileManager
+    {
+        private readonly Dictionary<string, byte[]> _files;
+
+        public InMemoryFileManager()
+        {
+            _files = new Dictionary<string, byte[]>();
+        }
+
+        public void SaveFile(byte[] data, string fileName)
+        {
+            if (_files.ContainsKey(fileName))
+            {
+                _files[fileName] = data;
+                return;
+            }
+
+            _files.Add(fileName, data);
+        }
+
+        public byte[] GetFile(string fileName)
+        {
+            if (!_files.TryGetValue(fileName, out var fileData))
+            {
+                return null;
+            }
+
+            return fileData;
+        }
+
+        public void DeleteFile(string fileName)
+        {
+            if (_files.ContainsKey(fileName))
+            {
+                _files.Remove(fileName);
+            }
+        }
+
+        public void DeleteAllFiles()
+        {
+            _files.Clear();
+        }
+    }
+}
