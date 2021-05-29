7bc61135c7b6c3193c2462f221569b36b8f68d60
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 10:47:36 +0200

    Add IDummyData service interface

---
+++ src/FileExchanger/FileExchanger/Services/DummyData/IDummyDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.DummyData
+{
+    public interface IDummyDataService
+    {
+        bool SendDummyDataOnly { get; }
+        bool ReceiveDummyDataOnly { get; }
+        string KeyDummyData { get; }
+        void SetSendDummyDataOnly();
+        void SetReceiveDummyDataOnly();
+        string GetStringDummyData();
+        byte[] GetBytesDummyData();
+    }
+}
