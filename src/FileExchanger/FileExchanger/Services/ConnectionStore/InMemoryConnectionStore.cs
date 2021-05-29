5798caedf0013aefc313e986e2c48f4f0d5a2c3d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 11:12:43 +0200

    Add InMemoryConnectionStore

---
+++ src/FileExchanger/FileExchanger/Services/ConnectionStore/InMemoryConnectionStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.ConnectionStore
+{
+    public class InMemoryConnectionStore : IConnectionStore
+    {
+        public string HostId { get; set; }
+        public void SetHostId(string hostId)
+        {
+            HostId = hostId;
+        }
+    }
+}
