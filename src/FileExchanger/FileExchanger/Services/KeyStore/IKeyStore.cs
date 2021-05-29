f6ad4465318433ec1388184f5e9a35e287e9315f
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 11:01:11 +0200

    Add IKeyStore interface

---
+++ src/FileExchanger/FileExchanger/Services/KeyStore/IKeyStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.KeyStore
+{
+    public interface IKeyStore
+    {
+        void SaveEncryptedKeys(string publicKey, string privateKey, string hashedPassword);
+        (string PublicKey, string PrivateKey) GetEncryptedKeys(string hashedPassword);
+        bool KeysExists();
+        void DeleteKeys();
+    }
+}
