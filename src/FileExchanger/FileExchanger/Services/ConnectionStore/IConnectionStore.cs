f27c5e256122bbe55e91c98be3f98ff94a3c5ade
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 11:12:32 +0200

    Add IConnectionStore interface

---
+++ src/FileExchanger/FileExchanger/Services/ConnectionStore/IConnectionStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.ConnectionStore
+{
+    public interface IConnectionStore
+    {
+        string HostId { get; }
+        public void SetHostId(string hostId);
+    }
+}
