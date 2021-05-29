7ad679faca020a5acf357a2ccdcf0a597e684df2
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-29 11:01:33 +0200

    Add InMemory nad Directory KeyStore implementations

---
+++ src/FileExchanger/FileExchanger/Services/KeyStore/InMemoryKeyStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Services.KeyStore
+{
+    public class InMemoryKeyStore : IKeyStore
+    {
+        private string _privateKey = string.Empty;
+        private string _publicKey = string.Empty;
+        private string _hashedPassword = string.Empty;
+
+        public void SaveEncryptedKeys(string publicKey, string privateKey, string hashedPassword)
+        {
+            _privateKey = privateKey;
+            _publicKey = publicKey;
+            _hashedPassword = hashedPassword;
+        }
+
+        public void DeleteKeys()
+        {
+            _publicKey = string.Empty;
+            _privateKey = string.Empty;
+        }
+
+        public (string PublicKey, string PrivateKey) GetEncryptedKeys(string hashedPassword)
+        {
+            return _hashedPassword == hashedPassword ?
+                (_publicKey, _privateKey) :
+                (string.Empty, string.Empty);
+        }
+
+        public bool KeysExists()
+        {
+            return _privateKey != string.Empty && _publicKey != string.Empty;
+        }
+    }
+}
