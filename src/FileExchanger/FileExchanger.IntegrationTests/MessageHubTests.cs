1b809fcf27747314bc578d51be79b515bc50bc1c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-30 20:36:59 +0200

    Add message exchange test for first client

--- src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
+++ src/FileExchanger/FileExchanger.IntegrationTests/MessageHubTests.cs
@@ -568,6 +568,94 @@ namespace FileExchanger.IntegrationTests
         [InlineData(CipherMode.CBC)]
         [InlineData(CipherMode.CFB)]
         [InlineData(CipherMode.OFB)]
+        public async Task SendingText_WhenClientsExchangedKeys_FirstClientShouldReceiveCorrectMessage(CipherMode cipherMode)
+        {
+            // Arrange
+            var message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae metus eget sapien vulputate fermentum et ac tellus. Donec erat quam, bibendum eget consequat sit amet, egestas ut urna. Praesent felis nunc, fermentum id turpis vitae, congue sagittis erat. Duis iaculis urna non volutpat auctor. Maecenas a sapien varius, auctor ipsum quis, interdum elit. Nunc luctus ornare orci, ac porttitor magna ornare id. Vivamus pretium cursus velit eget vulputate. Vestibulum tellus sem, fringilla non pharetra ac, egestas a nisl. Nunc vitae lectus orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, ex ut porta ultricies, nisl arcu vehicula magna, eu laoreet elit elit auctor leo. Mauris eu blandit nunc. Proin hendrerit efficitur mattis. Proin blandit faucibus erat eu dictum. Nunc nec tortor pretium, auctor urna at, ullamcorper enim.\r\n\r\nVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam semper, augue et gravida maximus, tellus tellus tempus sem, hendrerit in.";
+            message = Encoding.UTF8.GetString(Encoding.UTF8.GetBytes(message).Take(1023).ToArray());
+            var receivedMessage = string.Empty;
+            var firstServer = BuildTestServer(clientName: "firstServer");
+            _firstServer = firstServer;
+            var secondServer = BuildTestServer(builder =>
+            {
+                builder.UseUrls("http://localhost:24555", "https://localhost:24556");
+            }, "secondServer");
+            _secondServer = secondServer;
+
+            var firstClient = await SetupConnectionsForBothServersAsync(firstServer, secondServer);
+            var firstClientPassword = "firstClientPassword";
+            var firstClientResetEvent = new ManualResetEvent(false);
+            var firstClientLoginResponse = await LoginClientAsync(firstClient.httpClient, firstClientPassword);
+            firstClient.hostConnection.On<bool>("ReceiveSendReceiverPublicKey", (b) =>
+            {
+                firstClientResetEvent.Set();
+            });
+            firstClient.clientConnection.On("ReceiveSendEncryptedSessionKey", () =>
+            {
+                firstClientResetEvent.Set();
+            });
+            firstClient.hostConnection.On<string>("ReceiveStopSendingMessage", (msg) =>
+            {
+                receivedMessage = msg;
+                firstClientResetEvent.Set();
+            });
+
+            var secondClient = await SetupConnectionsForBothServersAsync(secondServer, firstServer);
+            var secondClientPassword = "secondClientPassword";
+            var secondClientResetEvent = new ManualResetEvent(false);
+            var secondClientLoginResponse = await LoginClientAsync(secondClient.httpClient, secondClientPassword);
+            secondClient.clientConnection.On<bool>("ReceiveSendReceiverPublicKey", (b) =>
+            {
+                secondClientResetEvent.Set();
+            });
+            secondClient.hostConnection.On("ReceiveSendEncryptedSessionKey", () =>
+            {
+                secondClientResetEvent.Set();
+            });
+            
+
+            // Act
+            await secondClient.clientConnection.InvokeAsync(nameof(MessageHub.SendReceiverPublicKey), secondClientLoginResponse.PublicKey);
+            secondClientResetEvent.WaitOne();
+            firstClientResetEvent.WaitOne();
+            secondClientResetEvent.Reset();
+            firstClientResetEvent.Reset();
+
+            var response = await firstClient.httpClient.GetAsync("api/exchange/key");
+            var sessionKeyEncryptedWithPublicKey = await response.Content.ReadAsStringAsync();
+
+            await firstClient.clientConnection.InvokeAsync(nameof(MessageHub.SendEncryptedSessionKey),
+                sessionKeyEncryptedWithPublicKey);
+            secondClientResetEvent.WaitOne();
+            firstClientResetEvent.WaitOne();
+            secondClientResetEvent.Reset();
+            firstClientResetEvent.Reset();
+
+            response = await secondClient.httpClient.PostAsJsonAsync<PackageTextRequest>("api/exchange/packageText", new PackageTextRequest
+            {
+                Text = message,
+                Mode = cipherMode
+            });
+            var packages = await response.Content.ReadFromJsonAsync<List<Package>>();
+
+            await secondClient.clientConnection.InvokeAsync(nameof(MessageHub.StartSendingMessage), packages.Select(p => p.Id).ToList(),
+                packages.Count, cipherMode, 128);
+            foreach (var package in packages)
+            {
+                await secondClient.clientConnection.InvokeAsync(nameof(MessageHub.SendPackage), package);
+            }
+            await secondClient.clientConnection.InvokeAsync(nameof(MessageHub.StopSendingMessage));
+            firstClientResetEvent.WaitOne();
+
+            // Assert
+            receivedMessage.Should().Be(message);
+        }
+
+        [Theory]
+        [InlineData(CipherMode.ECB)]
+        [InlineData(CipherMode.CBC)]
+        [InlineData(CipherMode.CFB)]
+        [InlineData(CipherMode.OFB)]
         public async Task SendingText_WhenFirstClientNotAuthenticatedAndClientsExchangedKeys_SecondClientShouldReceiveDummyMessage(CipherMode cipherMode)
         {
             // Arrange
