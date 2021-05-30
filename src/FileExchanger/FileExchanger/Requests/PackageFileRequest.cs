db5023a284cd83cea18bbfa3bd3850f56f6e8623
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-05-30 19:50:36 +0200

    Add request/response classes

---
+++ src/FileExchanger/FileExchanger/Requests/PackageFileRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Requests
+{
+    public class PackageFileRequest
+    {
+        public CipherMode Mode { get; set; } = CipherMode.CBC;
+    }
+}
---
+++ src/FileExchanger/FileExchanger/Responses/LoginResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileExchanger.Responses
+{
+    public class LoginResponse
+    {
+        public string PublicKey { get; set; }
+    }
+}
